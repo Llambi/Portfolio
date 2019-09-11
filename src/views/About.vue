08f543d1b543d298ea2a1ca297b5b08a489f0ff7
Hugo <hugopf.mail@gmail.com>
2019-09-11 15:08:49 +0200

    [FEATURE] Realizada vista About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,27 @@
 <template>
     <v-app id="inspire">
         <v-app-bar
+                class="amber accent-4 black--text"
                 app
                 clipped-left
         >
-            <v-toolbar-title>Hugo Pérez Fernández</v-toolbar-title>
+            <v-toolbar-title class="black--text">
+                <router-link to="/" class="toolbar-title toolbar__title">
+                    Hugo Pérez Fernández
+                </router-link>
+            </v-toolbar-title>
+            <div class="flex-grow-1"></div>
+            <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                    <v-btn
+                            icon
+                            to="/"
+                    >
+                        <v-icon color="black">fas fa-caret-left</v-icon>
+                    </v-btn>
+                </template>
+                <span>Regress</span>
+            </v-tooltip>
         </v-app-bar>
 
         <v-content>
@@ -16,21 +33,87 @@
                         align="center"
                         justify="center"
                 >
-                    <v-col class="shrink">
-                        <v-tooltip right>
-                            <template v-slot:activator="{ on }">
+                    <v-col
+                            class="shrink"
+                            v-for="(repo,id) in repos"
+                            :key="id"
+                            sm="4"
+                            cols="4"
+                    >
+                        <v-card class="elevation-10">
+                            <v-toolbar
+                                    color="amber accent-4"
+                                    class="black--text"
+                                    flat
+                            >
+                                <v-toolbar-title>
+                                    <div class="font-weight-bold">{{repo.name}}</div>
+                                    <span class="black--text subtitle-1">Created: {{repo.created_at}}   Updated: {{repo.pushed_at}}</span>
+                                </v-toolbar-title>
+                            </v-toolbar>
+                            <v-card-actions class="d-flex justify-space-between mb-6">
+                                <v-tooltip bottom>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn
+                                                icon
+                                                large
+                                                :href=repo.html_url
+                                                target="_blank"
+                                                v-on="on"
+                                        >
+                                            <v-icon>fab fa-github</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span>{{repo.name}}</span>
+                                </v-tooltip>
+                                <v-tooltip bottom>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn
+                                                icon
+                                                large
+                                                target="_blank"
+                                                v-on="on"
+                                        >
+                                            <v-icon>{{repo.icon}}</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span>{{repo.lenguaje}}</span>
+                                </v-tooltip>
+                                <v-tooltip bottom>
+                                    <template v-slot:activator="{ on }">
+                                        <v-btn
+                                                icon
+                                                large
+                                                target="_blank"
+                                                v-on="on"
+                                        >
+                                            <v-icon>fas fa-glasses</v-icon>
+                                        </v-btn>
+                                    </template>
+                                    <span>{{repo.watchers}}</span>
+                                </v-tooltip>
                                 <v-btn
-                                        :href="source"
+                                        color="amber accent-4"
                                         icon
-                                        large
-                                        target="_blank"
-                                        v-on="on"
+                                        @click="repo.show = !repo.show"
                                 >
-                                    <v-icon large>mdi-code-tags</v-icon>
+                                    <v-icon>{{ repo.show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
                                 </v-btn>
-                            </template>
-                            <span>Source</span>
-                        </v-tooltip>
+                            </v-card-actions>
+                            <v-expand-transition>
+                                <div v-show="repo.show">
+                                    <v-divider></v-divider>
+                                    <v-subheader
+                                            class="white--text"
+                                    >
+                                        Description:
+                                    </v-subheader>
+                                    <v-card-text>
+                                        {{repo.description}}
+                                    </v-card-text>
+                                </div>
+                            </v-expand-transition>
+                        </v-card>
                     </v-col>
                 </v-row>
             </v-container>
@@ -46,7 +129,7 @@
             source: String,
         },
         data: () => ({
-          repos:{}
+            repos: [],
         }),
         created() {
             this.$vuetify.theme.dark = true;
@@ -56,13 +139,42 @@
             axios
                 .get(GIT_API_URL + "/users/" + USER + "/repos")
                 .then((res) => {
-                    // eslint-disable-next-line no-console
                     res.data.forEach((x) => {
                         axios
                             .get(GIT_API_URL + "/repos/" + USER + "/" + x['name'])
-                            .then()
+                            .then((res) => {
+                                let data = res.data;
+                                let repo = {
+                                    name: data.name,
+                                    description: data.description,
+                                    html_url: data.html_url,
+                                    lenguaje: data.lenguaje === undefined ? "Unknow" : data.lenguaje,
+                                    icon: (data.lenguaje === undefined ? "fas fa-question-circle" : "fab fa-" + data.lenguaje).toLowerCase(),
+                                    watchers: data.watchers,
+                                    created_at: data.created_at.split("T")[0],
+                                    pushed_at: data.pushed_at.split("T")[0],
+                                    show: false,
+                                };
+                                this.repos.push(repo)
+                            })
                     })
                 })
         }
     }
 </script>
+
+<style>
+    .toolbar__title {
+        color: black;
+        text-decoration: none;
+    }
+    .toolbar__title:hover {
+        color: black;
+        text-decoration: none;
+    }
+    .toolbar__title:visited {
+        color: black;
+        text-decoration: none;
+    }
+
+</style>
